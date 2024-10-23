a34ea0e1026a925b1ce1ada205e5f3df360cdcfd
simon-builder <simonma.devs@gmail.com>
2024-10-23 21:27:49 +0200

    test push

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* Add Hero Images Heres */}
           <Image
             src="/hero-desktop.png"
             width={1000}
