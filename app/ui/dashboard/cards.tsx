e26ebe3fcb07c3c7d1d791ee40151937e57a56f2
simon-builder <simonma.devs@gmail.com>
2024-10-24 19:47:45 +0200

    chapter 8 part 1

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -3,8 +3,8 @@ import {
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+} from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,22 +13,20 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
-  return (
-    <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
-    </>
-  );
-}
+// export default async function CardWrapper() {
+//   return (
+//     <>
+//       <Card title="Collected" value={totalPaidInvoices} type="collected" />
+//       <Card title="Pending" value={totalPendingInvoices} type="pending" />
+//       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+//       <Card
+//         title="Total Customers"
+//         value={numberOfCustomers}
+//         type="customers"
+//       />
+//     </>
+//   );
+// }
 
 export function Card({
   title,
@@ -37,7 +35,7 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  type: "invoices" | "customers" | "pending" | "collected";
 }) {
   const Icon = iconMap[type];
 
