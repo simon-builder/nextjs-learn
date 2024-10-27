cd09947a281ab1c8f0a17b937c3d88656b9e5afb
simon-builder <simonma.devs@gmail.com>
2024-10-27 07:32:01 +0100

    customised metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Acme Dashboard",
-    default: "Acme Dashboard",
+    template: "%s | Simon's Dashboard",
+    default: "Simon's Dashboard",
   },
   description: "The official Next.js Learn Dashboard built with App Router.",
   metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
